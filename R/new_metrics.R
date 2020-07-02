d64014bb23e80bee25fcc6d2620530ceeb5a51cf
Irina Gaynanova <irinag@stat.tamu.edu>
2020-07-02 15:06:48 -0500

    New metric added

---
+++ R/new_metrics.R
@@ -0,0 +1,25 @@
+
+
+#' Function 1
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+new_function1 <- function(x){
+  mean(x)
+}
+
+#' Function 2
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+new_function2 <- function(x){
+  sd(x)
+}
